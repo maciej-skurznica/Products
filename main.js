116f61a6a6fca8fb6d679fb8fe95df2b7ca368d4
Maciej Skurznica <maciej.skurznica@gmail.com>
2023-05-26 12:54:37 +0100

    make website fully accessible

--- main.js
+++ main.js
@@ -92,8 +92,8 @@ const displayProducts = (list) => {
     productCard.classList.add("grid-item");
     productCard.innerHTML = `
       <div class="item">
-        <img class="item-image" src="./assets/images/Rectangle ${id}.png" alt="item ${name} image" />
-        <img class="item-arrow" src="./assets/images/arrow.png" alt="small arrow" />
+        <img class="item-image" src="./assets/images/Rectangle ${id}.png" alt="${name}" />
+        <img class="item-arrow" src="./assets/images/arrow.png" alt="more details about ${name}" />
       </div>
       <h3 class="grid-item-title">${name}</h3>
       <p class="grid-item-price">£${price}</p>
